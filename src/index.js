f7ec73fd962c6cdd1a8157a4f65877e87b7f13e8
sonupk <sonukamble1121@gmail.com>
2022-08-12 20:16:24 +0530

    assignment voting player

--- src/index.js
+++ src/index.js
@@ -2,9 +2,24 @@ const express = require('express');
 const bodyParser = require('body-parser');
 const route = require('./routes/route.js');
 const app = express();
+const { default:mongoose}=require('mongoose');
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
+
+mongoose.connect("mongodb+srv://sonupk:1HivF6DXHWanVcYu@cluster0.vtjazgb.mongodb.net/Sonu52-DB?retryWrites=true&w=majority"
+,{
+    useNewUrlParser: true
+}
+
+)
+.then( () => console.log("Mongodb is now Connected"))
+.catch(err => console.log(err))
+
+
+
+
+
 app.use('/', route);
 
 app.listen(process.env.PORT || 3000, function() {
--- src/routes/route.js
+++ src/routes/route.js
@@ -70,4 +70,120 @@ router.post("/test-post-4", function(req, res) {
     res.send(  { msg: arr , status: true }  )
 })
 
+
+// Players assignement solution here
+let players = [
+    {
+      name: "manish",
+      dob: "1/1/1995",
+      gender: "male",
+      city: "jalandhar",
+      sports: ["swimming"],
+    },
+    {
+      name: "gopal",
+      dob: "1/09/1995",
+      gender: "male",
+      city: "delhi",
+      sports: ["soccer"],
+    },
+    {
+      name: "lokesh",
+      dob: "1/1/1990",
+      gender: "male",
+      city: "mumbai",
+      sports: ["soccer"],
+    },
+  ];
+
+  router.post("/players", function (req, res) {
+    let PlayersName = req.body.name
+    let isNameRepeated = false
+
+
+  for(let i=0;i<players.length;i++){
+  if(players[i].name==PlayersName){
+      isNameRepeated=true
+      break
+  }
+  }
+  if(isNameRepeated){
+     res.send("player exists already")
+  }else{
+    console.log(req.body)
+    players.push(req.body)
+    res.send(players)
+  }
+  
+});
+
+// you will be given an array of persons (1.e an array of objects)..each person will have a 
+//(name: String, age: Number, yatingStatus: true/false(Boolean)} take input in query param as votingAge..
+//and for all the people above that age, change votingStatus as true also return an array consisting of only
+// the person that can vote
+
+// WRITE A POST API TO THE ABOVE
+
+// take this as sample for array of persons: 
+
+
+ let persons = [
+
+   {
+         nane: "PK",
+
+         age: 10,
+         votingStatus: false
+     },
+
+    {
+        nane: "SK",
+
+         age: 20,
+
+         votingStatus: false
+     },
+
+     {
+         nane: "AA", age: 70,
+
+         votingStatus: false
+     },
+
+     {
+         nane: "SC",
+
+         age: 5, votingStatus: false
+     },
+
+     {
+         name: "HO",
+
+         age: 40,
+
+         votingStatus: false
+     }
+ ]
+
+router.post("/voting_persion",function(req,res){
+    let input =req.query
+    let votingAge=input.age
+    let filterPersions=[]
+    for(let i in persons){
+        if(persons[i].age>votingAge){
+            persons[i].votingStatus=true
+            filterPersions.push(persons [i])
+        }
+    }
+
+
+    res.send({data:filterPersions,status:true})
+
+
+    
+    
+        
+   
+})
+
 module.exports = router;
